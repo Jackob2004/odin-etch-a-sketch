a605125d577debbbfa2a5b52995d1c4dcbd6ca51
Jackob2004 <zjakub2004@gmail.com>
2025-07-18 10:00:30 +0200

    Add changing dimension using range slider
    Add range slider.
    Add buttons to change dimension by one.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const BASE_GRID_SIZE = 600;
 const HEX_CHARACTERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 const gridContainer = document.querySelector('.grid-container');
 
+const dimensionInput = document.querySelector('.dimension-control input');
+const dimensionLabel = document.querySelector('.dimension-control label');
+
 function calculateElementSize(elementsInRow) {
     const GRID_ELEMENT_BORDER= 2;
     return BASE_GRID_SIZE / elementsInRow - GRID_ELEMENT_BORDER;
@@ -48,19 +51,32 @@ function changeGridElementColor(event) {
 }
 
 function regenerateGrid() {
-    const newRowSize = +prompt('Enter new gird\'s dimension [1-100]', '');
-    if (!newRowSize || !Number.isInteger(newRowSize)) return;
-
-    let validatedRowSize = newRowSize;
-
-    validatedRowSize = (validatedRowSize > 100) ? 100 : validatedRowSize;
-    validatedRowSize = (validatedRowSize < 1) ? 1 : validatedRowSize;
+    const updatedDimension = dimensionInput.value;
 
     document.querySelectorAll('.grid-container div').forEach((node) => node.remove());
-    createGridElements(validatedRowSize);
+    createGridElements(updatedDimension);
+}
+
+function updateInputLabel() {
+    const currentValue = dimensionInput.value;
+    dimensionLabel.textContent = currentValue + "x" + currentValue;
+}
+
+function updateDimensionByOne(value) {
+    const currDimension = +dimensionInput.value;
+    dimensionInput.value = currDimension + value;
+
+    updateInputLabel();
+    regenerateGrid();
 }
 
 createGridElements();
 
 gridContainer.addEventListener('mouseover', changeGridElementColor);
-document.querySelector('button').addEventListener('click', regenerateGrid);
+
+dimensionInput.addEventListener('change', regenerateGrid);
+dimensionInput.addEventListener('input', () => updateInputLabel());
+
+document.querySelector('.subtract').addEventListener('click', () => updateDimensionByOne(-1));
+document.querySelector('.addition').addEventListener('click', () => updateDimensionByOne(1));
+// TODO: ability to change between black/rgb/eraser, change colors on hover + left click down, better looking UI
